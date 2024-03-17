cefcac94a2fa06afdb2bea6e186920c8ef9b8e7d
caron2002 <qkrtkddnr122@gmail.com>
2024-03-17 23:51:54 +0900

    Console, 변수설정

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-alert("hi");
+//console.log(); 콘솔에 값을 출력
+// const 변수 설정
+// let 변수 설정
+// const 와 let의 차이 const 로 생성한 변수는 업데이트 할 수없다.
+// let
+const a = 5;
+const b = 2;
+let myName = "caron";
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello" + myName);
+
+myName = "david"
+console.log("your new name is " + myName);
