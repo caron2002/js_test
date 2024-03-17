640e6629eaf2860d89711537e9ce8be73c9542ba
caron2002 <qkrtkddnr122@gmail.com>
2024-03-17 23:55:19 +0900

    프린트&변수설정

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 //console.log(); 콘솔에 값을 출력
-// const 변수 설정
-// let 변수 설정
+// const, let 변수 설정
 // const 와 let의 차이 const 로 생성한 변수는 업데이트 할 수없다.
-// let
+// 주로 const 를 사용
+
+
 const a = 5;
 const b = 2;
 let myName = "caron";
