afb38fbf487fa5a3d286799fbd80e76a363aa3ec
caron2002 <qkrtkddnr122@gmail.com>
2024-03-19 19:25:09 +0900

    Nomad Coders #3

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-const calcultor = {
-  add: function(a, b){
-    console.log(a + b);
-  },
-  minus: function(a, b){
-    console.log(a - b);
-  },
-  div: function(a, b){
-    console.log(a / b);
-  },
-  multi: function(a, b){
-    console.log(a * b);
-  },
-};
+//NaN == Not a Number
+// if문 condition은 boolean이여야 한다
+// and 연산자 &&, or 연산자 ||
+const age = parseInt(prompt("How old are you?"));
+//parseInt("문자") => NaN 값을 준다
+if (isNaN(age) || age < 0) {
+  age2 = parseInt(prompt("Please write a real positive number"));
+  if (!isNaN(age2)) { // ! == not의 개념
+    if (age2 < 18) {
+      console.log("You are too young.");
+    } else if (age2 < 50) {
+      console.log("you great");
+    } else {
+      console.log("you are too living.");
+    }
+    
+  }
+} else {
+  console.log("Thank you for writing your age")
+}
 
-calcultor.add(2, 3);
-calcultor.minus(23, 58);
-calcultor.div(234, 54);
-calcultor.multi(23, 45);
