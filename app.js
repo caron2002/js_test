d56aaaeb19663de91839fe9328d00beed0972fdd
caron2002 <qkrtkddnr122@gmail.com>
2024-03-20 22:41:57 +0900

    Nomardcoder #4

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
-//NaN == Not a Number
-// if문 condition은 boolean이여야 한다
-// and 연산자 &&, or 연산자 ||
-const age = parseInt(prompt("How old are you?"));
-//parseInt("문자") => NaN 값을 준다
-if (isNaN(age) || age < 0) {
-  age2 = parseInt(prompt("Please write a real positive number"));
-  if (!isNaN(age2)) { // ! == not의 개념
-    if (age2 < 18) {
-      console.log("You are too young.");
-    } else if (age2 < 50) {
-      console.log("you great");
-    } else {
-      console.log("you are too living.");
-    }
-    
-  }
-} else {
-  console.log("Thank you for writing your age")
-}
+const h1 = document.querySelector(".hello h1");
 
+function handleTitleClick() {
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
+    
+  } else {
+    newColor = "blue";
+  }
+  h1.style.color = newColor;
+}
+h1.addEventListener("click", handleTitleClick);
